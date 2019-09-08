1a33f4c6b26d0b8b700caf08afd413a7e1e485d1
123wdx <1794519835@qq.com>
2019-09-08 19:21:43 +0800

    魏等霞修改了网页

--- ej/js/index.js
+++ ej/js/index.js
@@ -12,4 +12,13 @@ $(function(){
 	$("ul.left_nav > li:first-child").trigger("click");
 
 
-});
+	});
+$(function(){
+	$("div.mb_nav").on("click","a",function(){
+		$("div.mb_nav>a").removeClass("current");
+		$(this).addClass("current");
+		var url=$(this).attr("url");
+		$("#wrapper").load(url);
+		$("div.mb_nav > a:first-child").trigger("click");
+	})
+})
